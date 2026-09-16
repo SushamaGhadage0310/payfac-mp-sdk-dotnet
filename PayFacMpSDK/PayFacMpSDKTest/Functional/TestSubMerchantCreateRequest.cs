b25eca9316123bc228ef4f0607a05e0e40f7a67d
Sushama Ghadage <159752725+SushamaGhadage0310@users.noreply.github.com>
2026-09-16 17:14:05 +0530

    Add ValueAddedServices for 15.x (#27)
    * commiting changes for 'ValueAddedServices' in version 15.x
    
    * commiting changes for removing extra changes
    
    ---------
    
    Co-authored-by: e5651806 <sushamaghadage38@gmail.com>

--- PayFacMpSDK/PayFacMpSDKTest/Functional/TestSubMerchantCreateRequest.cs
+++ PayFacMpSDK/PayFacMpSDKTest/Functional/TestSubMerchantCreateRequest.cs
@@ -566,6 +566,11 @@ namespace PayFacMpSDKTest.Functional
                 complianceProducts = new complianceProducts
                 {
                     productField = new System.Collections.Generic.List<complianceProductsList>()
+                },
+
+                valueAddedServices = new valueAddedServices
+                {
+                    serviceField = new System.Collections.Generic.List<serviceList>()
                 }
             };
 
@@ -580,6 +585,11 @@ namespace PayFacMpSDKTest.Functional
 
 
             request.complianceProducts.productField.Add(newProduct);
+
+            var newService = new serviceList();
+            newService.code = valueAddedServiceProductCode.DISPUTE_DEFENDER;
+            newService.enabled = true;
+            request.valueAddedServices.serviceField.Add(newService);
 
             var categoryType = new string("GC");
             //var categoryType1 = new string("SM");
